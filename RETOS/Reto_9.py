abadea23555d3f0b3f48396e3c272b0541d07d35
Jaime <jatona27@gmail.com>
2021-11-13 21:46:39 +0100

    Reto 9  (easy) completado

---
+++ RETOS/Reto_9.py
@@ -0,0 +1,12 @@
+#Escribe un programa que pueda decirte si un año (número entero) es bisiesto o no
+
+año = int(input('Introduzca un año: ')) #el año que queremos comprobar
+
+if año % 4 != 0: #no divisible entre 4
+	print("No es bisiesto")
+elif año % 4 == 0 and año % 100 != 0: #divisible entre 4 y no entre 100 o 400
+	print("Es bisiesto")
+elif año % 4 == 0 and año % 100 == 0 and año % 400 != 0: #divisible entre 4 y 10 y no entre 400
+	print("No es bisiesto")
+elif año % 4 == 0 and año % 100 == 0 and año % 400 == 0: #divisible entre 4, 100 y 400
+	print("Es bisiesto")
